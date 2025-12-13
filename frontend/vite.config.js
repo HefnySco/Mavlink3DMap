8b65911ac0ad77aa9c0e88e3f48ac2ca2460c500
HefnySco <mohammad.hefny@gmail.com>
2025-12-13 13:54:19 +0300

    fix build

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,12 +3,26 @@ import { resolve } from 'path'
 
 export default defineConfig({
   build: {
+    chunkSizeWarningLimit: 1500,
     rollupOptions: {
       input: {
         index: resolve(__dirname, 'index.html'),
         index_4w: resolve(__dirname, 'index_4w.html'),
         index_4w_real_map: resolve(__dirname, 'index_4w_real_map.html'),
         index_4w_map_box: resolve(__dirname, 'index_4w_map_box.html'),
+      },
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('three')) {
+              return 'vendor_three';
+            }
+            if (id.includes('cannon-es')) {
+              return 'vendor_physics';
+            }
+            return 'vendor';
+          }
+        },
       },
     },
   },
